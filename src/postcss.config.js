bdcc31a2a0e60faba68bce746dac204e032820fd
lunaluna <web@lunalunadesign.com>
2019-03-20 11:39:58 +0900

    update env

---
+++ src/postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = () => ({
+  plugins: {
+    'autoprefixer': {
+      "browsers": "> 1% in JP, last 2 version, ie >= 11, iOS >= 9, Android >= 4.4",
+      "grid": true
+    }
+  }
+})
