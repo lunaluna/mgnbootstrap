ede92aa095cc7b1df2be049235cb133ca29768a4
lunaluna <web@lunalunadesign.com>
2019-04-10 17:04:24 +0900

    update / add editor-style.scss

--- functions/enqueue.php
+++ functions/enqueue.php
@@ -1,16 +1,27 @@
 <?php
+/*********************************/
+/*
+/* enqueue_scripts
+/*
+/*********************************/
+if ( !is_admin() ) {
+	add_action( 'wp_enqueue_scripts', function (){
+		wp_enqueue_style( 'swiper-style', get_theme_file_uri( '/assets/css/vender/swiper.min.css' ), array(), '' );
+		wp_enqueue_style( 'mgn-style', get_theme_file_uri( '/dist/css/style.css' ) );
+		wp_enqueue_style( 'editor-style', get_theme_file_uri( '/dist/css/editor-style.css' ) );
 
+		wp_enqueue_script( 'tile-js', get_theme_file_uri( '/assets/js/vender/jquery.tile.min.js' ), array( 'jquery' ), true, true );
+		wp_enqueue_script( 'ofi-js', get_theme_file_uri( '/assets/js/vender/ofi.min.js' ), array() 'jquery' , true, true );
+		wp_enqueue_script( 'picturefill-js', get_theme_file_uri( '/assets/js/vender/picturefill.min.js' ), array( 'jquery' ), true, true );
+		wp_enqueue_script( 'swiper-js', get_theme_file_uri( '/assets/js/vender/swiper.min.js' ), array( 'jquery' ), true, true );
+		wp_enqueue_script( 'mgn-scripts', get_theme_file_uri( '/dist/js/app.js' ), array( 'jquery' ), '', true );
+	});
+}
 
-add_action( 'wp_enqueue_scripts', function (){
-	wp_enqueue_style( 'swiper-style', get_theme_file_uri( '/assets/css/vender/swiper.min.css' ), array(), '' );
-	wp_enqueue_style( 'mgn-style', get_theme_file_uri( '/dist/css/style.css' ) );
-
-	wp_enqueue_script( 'tile-js', get_theme_file_uri( '/assets/js/vender/jquery.tile.min.js' ), array( 'jquery' ), true, true );
-	wp_enqueue_script( 'ofi-js', get_theme_file_uri( '/assets/js/vender/ofi.min.js' ), array() 'jquery' , true, true );
-	wp_enqueue_script( 'picturefill-js', get_theme_file_uri( '/assets/js/vender/picturefill.min.js' ), array( 'jquery' ), true, true );
-	wp_enqueue_script( 'swiper-js', get_theme_file_uri( '/assets/js/vender/swiper.min.js' ), array( 'jquery' ), true, true );
-	wp_enqueue_script( 'mgn-scripts', get_theme_file_uri( '/dist/js/app.js' ), array( 'jquery' ), '', true );
-});
+add_action( 'enqueue_block_editor_assets', 'add_block_editor_style' );
+function add_block_editor_style() {
+	wp_enqueue_style( 'block-editor-style', get_theme_file_uri( '/dist/css/editor-style.css' ) );
+}
 
 
 add_action( 'after_setup_theme', function (){
